3e78c2c211222ce8f43b2e7075aa4bc9090c1116
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-05-02 11:22:40 -0500

    add relationship with users and role

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const auth = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(' ')[1];  //Bearer რომ მოცილდეს ტოკენიდან
     if (!token) {
         return res.status(401).json({ error: 'Unautorized' });
     }
